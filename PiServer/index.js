51b6b7587927e4dcf49bd4bb3785d5c612c30c6e
Adem Altan <adem.t.altan@gmail.com>
2020-09-22 10:28:39 +1000

    enabled 1 sec signal on pi

--- PiServer/index.js
+++ PiServer/index.js
@@ -1,18 +1,34 @@
 var express = require("express");
 var app = express();
 
-var gpiop = require('rpi-gpio').promise;
+var gpio = require('rpi-gpio').promise;
 
-gpiop.setup(22, gpiop.DIR_OUT)
+function openClose() {
+    gpio.setup(22, gpio.DIR_OUT)
 	    .then(() => {
-		return gpiop.write(22, true);              
+		gpio.write(22, true);
+                setTimeout(function() {
+                    close();
+                }, 1000);           
 	    })
 	    .catch((err) => {
 		console.log('Error: ', err.toString())
 	    });
+}
+
+function close() {
+    gpio.setup(22, gpio.DIR_OUT)
+	    .then(() => {
+	    return gpio.write(22, false);      
+	    })
+	    .catch((err) => {
+		console.log('Error: ', err.toString())
+	    });
+}
 
 app.get('/', (req, res) => res.status(200).json({ result: 'It Works on PI!' }));
 
-app.listen(3001, () => {
-    console.log("Server running on port 3001");
+app.listen(3005, () => {
+    console.log("Server running on port 3005");
+    openClose();
 });
