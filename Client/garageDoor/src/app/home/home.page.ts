c0a5c0721579aa16bbfa86cc5bff10947776908f
Adem <adem.altan@readify.net>
2020-09-25 18:50:55 +1000

    Enabled post request.

--- Client/garageDoor/src/app/home/home.page.ts
+++ Client/garageDoor/src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { IonDatetime } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -8,12 +10,21 @@ import { Component } from '@angular/core';
 export class HomePage {
   private isOpen = false;
   private garageDoorStatus = 'Unknown';
+  private url = 'http://dummy.restapiexample.com/api/v1/employees'; //'https://ademsgaragedoor.loca.lt/open';
 
   private openClose = () => {
     this.isOpen = !this.isOpen;
     this.garageDoorStatus = this.isOpen ? 'Open' : 'Close';
+    this.http.get<Response>(this.url).toPromise().then(response => {
+      console.log(response);
+    });
   }
 
-  constructor() {}
+  constructor(private http: HttpClient) { }
+}
 
+interface Response {
+  status: string;
+  dateTime: IonDatetime;
+  action: string;
 }
